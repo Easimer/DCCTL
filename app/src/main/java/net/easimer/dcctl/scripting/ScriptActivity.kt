fab17760e989ee85175ef1ebc688bddefcb7802c
Daniel Meszaros <easimer@gmail.com>
2020-06-13 03:48:30 +0200

    Add menu to scripting

--- app/src/main/java/net/easimer/dcctl/scripting/ScriptActivity.kt
+++ app/src/main/java/net/easimer/dcctl/scripting/ScriptActivity.kt
@@ -2,6 +2,7 @@ package net.easimer.dcctl.scripting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import net.easimer.dcctl.R
 
 class ScriptActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class ScriptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_script)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_script, menu)
+        return true
     }
 }
