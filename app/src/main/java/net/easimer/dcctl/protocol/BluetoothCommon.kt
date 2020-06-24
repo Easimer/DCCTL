d55e2b4b26b0b45785d8e3c328a4d86d50021b33
Daniel Meszaros <easimer@gmail.com>
2020-06-24 06:09:47 +0200

    Remove BluetoothServerStatisticsListener

--- app/src/main/java/net/easimer/dcctl/protocol/BluetoothCommon.kt
+++ app/src/main/java/net/easimer/dcctl/protocol/BluetoothCommon.kt
@@ -4,10 +4,6 @@ import net.easimer.dcctl.utils.Event
 
 const val BLUETOOTH_PROTOCOL_ID = "939a3570-763e-4ab2-a0de-94cd51759054"
 
-open class BluetoothServerStatisticsListener {
-    open fun onNumberOfScriptsReceivedChanged(numberOfScriptsReceived : Int) {}
-}
-
 interface IBluetoothServer {
     fun shutdown()
     val onScriptReceived : Event<Int>
