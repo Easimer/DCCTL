572b24e02ea9d5fd9dd88f109d065a10613e9f47
Daniel Meszaros <easimer@gmail.com>
2020-06-25 10:31:15 +0200

    Make the scripting screens try to turn on BT too

--- app/src/main/java/net/easimer/dcctl/ConfigActivity.kt
+++ app/src/main/java/net/easimer/dcctl/ConfigActivity.kt
@@ -1,8 +1,10 @@
 package net.easimer.dcctl
 
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.preference.PreferenceManager
@@ -11,7 +13,7 @@ import net.easimer.dcctl.protocol.broadcastScript
 import net.easimer.dcctl.scripting.Script
 import net.easimer.dcctl.scripting.Command
 
-class ConfigActivity : AppCompatActivity() {
+class ConfigActivity : ActivityResultCallbackActivity() {
     val vm = ConfigViewModel(10.0f, 1.0f, 0, false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,40 @@ class ConfigActivity : AppCompatActivity() {
         val cmdWait = Command.Wait(vm.delay)
         val cmdCapture = Command.CaptureMultiple(vm.interval, vm.count, vm.flash)
         val script = Script(listOf(cmdWait, cmdCapture))
+
+        checkBluetoothAndSendScript(script)
+    }
+
+    private fun checkBluetoothAndSendScript(script: Script, dialog: Boolean = false) {
+        BluetoothAdapter.getDefaultAdapter()?.let {
+            if(it.isEnabled) {
+                sendScript(script)
+            } else {
+                if(dialog) {
+                    // Tell the user why we need Bluetooth to be turned on
+                    AlertDialog.Builder(this)
+                        .setTitle(R.string.bluetooth_dialog_title)
+                        .setMessage(R.string.bluetooth_dialog_message)
+                        .setPositiveButton(R.string.bluetooth_dialog_retry) { _, _ ->
+                            // User clicked "retry", show the BT dialog again
+                            startActivityForResult(BluetoothAdapter.ACTION_REQUEST_ENABLE) {
+                                checkBluetoothAndSendScript(script, true)
+                            }
+                        }
+                        .setNegativeButton(R.string.bluetooth_dialog_ok) { _, _ ->
+                            // User acknowledged the fact that we can't function without bluetooth
+                        }
+                        .show()
+                } else {
+                    startActivityForResult(BluetoothAdapter.ACTION_REQUEST_ENABLE) {
+                        checkBluetoothAndSendScript(script, true)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun sendScript(script: Script) {
         val excludedDevicesDefault = HashSet<String>()
 
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
--- app/src/main/java/net/easimer/dcctl/scripting/ui/ScriptActivity.kt
+++ app/src/main/java/net/easimer/dcctl/scripting/ui/ScriptActivity.kt
@@ -1,13 +1,16 @@
 package net.easimer.dcctl.scripting.ui
 
+import android.bluetooth.BluetoothAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import net.easimer.dcctl.ActivityResultCallbackActivity
 import net.easimer.dcctl.Log
 import net.easimer.dcctl.R
 import net.easimer.dcctl.protocol.broadcastScript
@@ -16,7 +19,7 @@ import net.easimer.dcctl.scripting.Command
 import net.easimer.dcctl.scripting.SoundEffect
 import java.util.*
 
-class ScriptActivity : AppCompatActivity() {
+class ScriptActivity : ActivityResultCallbackActivity() {
     private lateinit var recyclerView : RecyclerView
     private lateinit var viewAdapter: CommandAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
@@ -92,7 +95,7 @@ class ScriptActivity : AppCompatActivity() {
             // then the user wants to do something special, like to send
             // the script.
             if(item.itemId == R.id.send) {
-                sendScript()
+                checkBluetoothAndSendScript()
                 return true
             } else {
                 return super.onOptionsItemSelected(item)
@@ -119,6 +122,35 @@ class ScriptActivity : AppCompatActivity() {
     /**
      * Broadcast the script that the user built.
      */
+    private fun checkBluetoothAndSendScript(dialog: Boolean = false) {
+        BluetoothAdapter.getDefaultAdapter()?.let {
+            if(it.isEnabled) {
+                sendScript()
+            } else {
+                if(dialog) {
+                    // Tell the user why we need Bluetooth to be turned on
+                    AlertDialog.Builder(this)
+                        .setTitle(R.string.bluetooth_dialog_title)
+                        .setMessage(R.string.bluetooth_dialog_message)
+                        .setPositiveButton(R.string.bluetooth_dialog_retry) { _, _ ->
+                            // User clicked "retry", show the BT dialog again
+                            startActivityForResult(BluetoothAdapter.ACTION_REQUEST_ENABLE) {
+                                checkBluetoothAndSendScript(true)
+                            }
+                        }
+                        .setNegativeButton(R.string.bluetooth_dialog_ok) { _, _ ->
+                            // User acknowledged the fact that we can't function without bluetooth
+                        }
+                        .show()
+                } else {
+                    startActivityForResult(BluetoothAdapter.ACTION_REQUEST_ENABLE) {
+                        checkBluetoothAndSendScript(true)
+                    }
+                }
+            }
+        }
+    }
+
     private fun sendScript() {
         val script = Script(script)
 
